ed8e6ed3b8aa1be242af5a1733657d1d7913198a
ranie <ranie.black29@gmail.com>
2019-09-16 19:22:33 -0300

    criado a classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,18 @@
 class Pessoa:
-    pass
+    def __init__(self, nome = None, idade = 33):
+        self.nome = None
+
+    def Cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('joao')
+    print(Pessoa.Cumprimentar(p))
+    print(id(p))
+    print(p.Cumprimentar())
+    print(p.nome)
+    p.nome = 'Ranie'
+    print(p.nome)
+    print(p.idade)
+
